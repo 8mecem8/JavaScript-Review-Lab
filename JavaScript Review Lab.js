25027155c105281bd3d256cc5dfa47acf59ea82c
Cem Mehmet Onal <mehmetcemonal@gmail.com>
2022-12-02 10:33:54 -0500

    :wrench:

---
+++ JavaScript Review Lab.js
@@ -0,0 +1,59 @@
+//  How do we assign a value to a variable?
+    //  The assignment operator (=)
+    //  let variable = value;
+// How do we change the value of a variable?
+    //  We can change the value of a variable with the different operators such as arithmetic (+, -, *, /)
+//  How do we assign an existing variable to a new variable?
+    //  The value of a variable can be assignment with the assignment operator (=)
+// Remind me, what are declare, assign, and define?
+    //  Variables can be declared with the var, let, and const keywords without a value
+    //  Variables can be assigned with the assignment operator (=)
+    //  Variables can be defined with the var, let, and const keywords with a value
+//  What is pseudocoding and why should you do it?
+    //  Writing code in human readable form without a specific syntax.
+    //  Pseudocode can be used to write detailed steps for creating an application with code.
+    //  Pseudocode can also be presented to non-developers to explain how the app is made and will work.
+//  What percentage of time should be spent thinking about how you're going to solve a problem vs actually typing in code to solve it?
+    //  50%
+
+    console.log("=====================================================\n\n")
+
+    //  =======================================================
+    //    B) Strings
+    //===========================================================
+    
+    
+    //  1.  Create a variable called firstVariable
+    let firstVariable;
+    
+    //  2.  Assign it the value of the string "Hello World"
+    firstVariable = "Hello World";
+    
+    //  3.  Change the value of this variable to some number
+    firstVariable = 6;
+    
+    //  4.  Store the value of firstVariable in a new variable called secondVariable
+    secondVariable = firstVariable;
+    
+    //  5.  Change the value of secondVariable to any string.
+    secondVariable = "Any string"
+    
+    //  6.  What is the value of firstVariable?
+    6
+    
+    //  7.  Create a variable called yourName and set it equal to your name as a string. 
+    //      Then, write an expression that takes the string "Hello, my name is " and the variable yourNameso that it returns a new string with them concatenated.
+    //      ex: Hello, my name is Jean Valjean
+    const yourName = "MeceM";
+    console.log(`Hello, my name is ${yourName}.`);
+    
+    
+    console.log("\n\n========================================================================\n\n")
+    
+    
+    
+    
+    //=======================================================
+    //    C) Booleans
+    //===========================================================
+    
